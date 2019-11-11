44898ad7012b01051c0f05c71f640127f6e0bee8
Arpan Abhishek <arpanforbusiness@gmail.com>
2019-11-12 00:01:55 +0530

    Polished UI and added constraint for starting the game

--- src/Board.java
+++ src/Board.java
@@ -2,15 +2,22 @@ public class Board {
     private int[][] snakes = {{36, 6}, {32, 10}, {48, 26}, {88, 24}, {62, 18}, {95, 56}, {97, 78}};
     private int[][] ladders = {{4, 14}, {8, 10}, {1, 38}, {21, 42}, {28, 76}, {50, 67}, {71, 92}, {88, 99}};
 
-    public void printBoard() {
+    public void printBoard(int player1Position, int player2Position) {
         int counter = 100;
         for (int i = 0; i < 10; i++) {
+            System.out.println("=========================================================================================================================");
+            System.out.print("|");
             for (int j = 0; j < 10; j++) {
-                System.out.print(counter + "\t");
+                String add = "   ";
+                if (player1Position == counter) add = add.trim() + "[1]";
+                if (player2Position == counter) add = add.trim() + "[2]";
+                System.out.print("\t" + counter + add + "\t|");
+
                 counter--;
             }
             System.out.println();
         }
+        System.out.println("=========================================================================================================================");
     }
 
     public boolean isSnake(int position) {
--- src/Main.java
+++ src/Main.java
@@ -13,36 +13,53 @@ public class Main {
 
         Board board = new Board();
         Dice dice = new Dice();
+        int rolledNumber;
+        boolean player1out = false;
+        boolean player2out = false;
         int lastTurn = 2;
+        System.out.println("So here is the snakes and ladders Board, enter 'roll' \n" +
+                " on your turn to roll the dice. Lets starts!");
 
-        System.out.println("So here is the snakes and ladders Board, press 'y' \n" +
-                " turn by turn to roll the dice. Lets starts!");
-        board.printBoard();
         while (true) {
-
-            String next = scan.nextLine();
-            if (next.equals("y")) {
+            String next;
                 if (lastTurn == 2) {
                     System.out.println(player1.getName() + "'s turn.");
-                    player1.changePosition(dice.roll());
-                    if (player1.getPosition() >= 100) {
-                        System.out.println(player1.getName() + " won!");
-                        break;
-                    }
-                    System.out.println(player1.getName() + " is at " + player1.getPosition());
-                    if (board.dec(player1.getPosition()) != -1) {
-                        System.out.println("A snake bit " + player1.getName() + " at " + player1.getPosition());
-                        player1.setPosition(board.dec(player1.getPosition()));
-                        System.out.println(player1.getName() + " is at " + player1.getPosition());
-                    } else if (board.inc(player1.getPosition()) != -1) {
-                        System.out.println(player1.getName() + " got a ladder at " + player1.getPosition());
-                        player1.setPosition(board.inc(player1.getPosition()));
-                        System.out.println(player1.getName() + " is at " + player1.getPosition());
+                    next = scan.nextLine();
+                    if (next.equals("roll")) {
+                        rolledNumber = dice.roll();
+                        System.out.println(player1.getName() + " rolled " + rolledNumber);
+                        if (rolledNumber == 6 || player1out) {
+                            player1out = true;
+                            player1.changePosition(rolledNumber);
+                            if (player1.getPosition() >= 100) {
+                                System.out.println(player1.getName() + " won!");
+                                break;
+                            }
+                            System.out.println(player1.getName() + " is at " + player1.getPosition());
+                            if (board.dec(player1.getPosition()) != -1) {
+                                System.out.println("A snake bit " + player1.getName() + " at " + player1.getPosition());
+                                player1.setPosition(board.dec(player1.getPosition()));
+                                System.out.println(player1.getName() + " is at " + player1.getPosition());
+                            } else if (board.inc(player1.getPosition()) != -1) {
+                                System.out.println(player1.getName() + " got a ladder at " + player1.getPosition());
+                                player1.setPosition(board.inc(player1.getPosition()));
+                                System.out.println(player1.getName() + " is at " + player1.getPosition());
+                            }
+                        } else {
+                            System.out.println("You can only play after rolling one 6.");
+                            System.out.println(player1.getName() + " is at " + player1.getPosition());
+                        }
                     }
                     lastTurn = 1;
                 } else {
                     System.out.println(player2.getName() + "'s turn.");
-                    player2.changePosition(dice.roll());
+                    next = scan.nextLine();
+                    if (next.equals("roll")) {
+                        rolledNumber = dice.roll();
+                        System.out.println(player2.getName() + " rolled " + rolledNumber);
+                        if (rolledNumber == 6 || player2out) {
+                            player2out = true;
+                            player2.changePosition(rolledNumber);
                     if (player2.getPosition() >= 100) {
                         System.out.println(player2.getName() + " won!");
                         break;
@@ -57,9 +74,14 @@ public class Main {
                         player2.setPosition(board.inc(player2.getPosition()));
                         System.out.println(player2.getName() + " is at " + player2.getPosition());
                     }
+                        } else {
+                            System.out.println("You can only play after rolling one 6.");
+                            System.out.println(player2.getName() + " is at " + player2.getPosition());
+                        }
                     lastTurn = 2;
                 }
             }
+            board.printBoard(player1.getPosition(), player2.getPosition());
         }
     }
 }
